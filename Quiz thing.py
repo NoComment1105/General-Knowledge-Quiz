60e21c6b66f5610d25f55c2a31b2961cee2d1e5b
NoComment1105 <purpleunicorn011105@outlook.com>
2020-12-10 17:25:30 +0000

    Added system that was originally wanted for showing previous highscores

--- Quiz thing.py
+++ Quiz thing.py
@@ -1,11 +1,15 @@
 #This is a quiz that takes in data of each player and records their highscore
 print("Good Morning, Good evening, Good night, Good Day, Welcome to this fabulous general knowledge quiz i have concocted. It asks for your name and some other things and stores them so you can track your high score even when you close the program")
+players=[]
+with open("highscores.txt","r") as InFile:
+     line=InFile.readline()
+     while line:
+          players.append(line.rstrip("\n").split(","))
+          line=InFile.readline()
 name = input("Please enter your name ")
 age = input("Please enter your age ")
 shoeSize = input("Enter your shoe size (yes) ")
-with open("highscores.txt","a") as file1:
-     file1.write(name + ", " + age + ", " +  shoeSize + ", ")
-     file1.close
+
 
 counter = 0
 print("In all questions start it with a capital, and grammar and stuff \n")
@@ -17,7 +21,7 @@ else:
      print("Unlucky, Incorrect. The answer is 1912\n")
 
 answerQ2 = input("What is the Chemical Symbol for Silver? is it a) S   b) K  c) Ag (answer with the letter) ")
-if answerQ2 == "C" or "c":
+if answerQ2 == "c":
      print("Correct\n")
      counter = counter + 1
 else:
@@ -63,41 +67,32 @@ if answerQ8 == "west":
      print("Correct\n")
      counter = counter + 1
 else:
-     print("Unlucky. The answer is west")
+     print("Unlucky. The answer is west\n")
 
 answerQ9 = int(input("How many aces are there in a deck of cards? (enter integer)"))
 if answerQ9 == 4:
-     print("Well done, Correct")
+     print("Well done, Correct\n")
      counter = counter + 1
 else:
      print("Incorrect, the answer is 4, spades hearts clubs and diamonds")
 
 answerQ10 = input("Round and round the ______ like a teddy bear one step two step tickly under there")
 if answerQ10 == "garden":
-     print("Correct :)")
+     print("Correct :)\n")
      counter = counter + 1
 else:
      print("Incorrect, the answer is garden")
 
-answerQ11 = input("What is longer, a Nautical mile or a mile?")
-if answerQ11 == "Nautical mile":
-     print("Correct! a Nautical Mile is 1.15 miles")
-     counter = counter + 1
-else:
-     print("Incorrect, a Nautical mile is longer, it is 1.15 miles")
-
-answerQ12 = input("Ben Stokes inspired England’s 2019 Cricket World Cup final victory over New Zealand – who scored the second-highest number of runs in the match for England?")
-if answerQ12 == "Jos Buttler":
-     print("Correct, it was a spectacular display of cricket")
-     counter = counter + 1
-else:
-     print("Unlucky, the answer is Jos Buttler")
-
-
 
 counterstr = str(counter)
-with open("highscores.txt","a") as file:
-     file.write(counterstr + "\n")
+with open("highscores.txt","a") as file1:
+               file1.write(name+","+age+","+shoeSize+","+counterstr+"\n")
+               
+print("Well Done! You have completed this quiz; you scored:", counter, "Whoop!")
 
-print("Well Done! You have completed this quiz; you scored:", counter, "Whoop! If this quiz has been played multiple times, navigate to where this python program has been saved to find the .txt file where all data is being stored")
-
+for i in range(len(players)):
+     if players[i][0] == name:
+          print("Your previous highscore was",players[i][3])
+          print("This time you scored", counter)
+     else:
+          toe = 1
